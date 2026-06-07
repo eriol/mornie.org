88a9f947577e4e847bfa8f5eeb3b6f412907e456
Daniele Tricoli <eriol@mornie.org>
2026-06-07 04:09:20 +0200

    Add math support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 export default defineConfig({
   site: 'https://mornie.org',
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       theme: 'monokai',
     },
